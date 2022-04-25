1b3a5295327524e1b61af0f50f67772a8ca94658
Mauriciiow <mauricioliveira639@gmail.com>
2022-04-24 23:04:17 -0300

    adicionando validacoes

--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -11,6 +11,11 @@ import  express  from "express";
     
         try {
             const clientes = await Clientes.find() 
+
+            if (!clientes) {
+                res.status(500)
+                return
+            }
             res.status(200).json({"clientes": clientes})
         } catch (error) {
             res.status(400).json({"erro": true, 'msg': error.message })
@@ -23,8 +28,12 @@ import  express  from "express";
         try {
             const id = req.params.id
            const clienteEncontrado = await Clientes.findById(id)
+           if (!clienteEncontrado) {
+               res.status(404).json({"message": "cliente nao encontrado", "erro": true})
+               return
+           }
     
-            res.status(200).json({"msg": "cliente encontrado", "cliente": clienteEncontrado})
+            res.status(200).json({"message": "cliente encontrado", "cliente": clienteEncontrado})
         } catch (error) {
             res.status(400).json({"erro": true, 'msg': error.message })
         }
@@ -38,20 +47,20 @@ import  express  from "express";
             const email = req.body.email
             const testaEmail = await Clientes.find({email: email})
           
-            if (testaEmail.length == 0) {
-                const clienteNovo = new Clientes({
-                    nome: body.nome,
-                    data_nascimento: body.data_nascimento,
-                    cpf: body.cpf,
-                    email: body.email,
-                    senha: body.senha,
-                    genero: body.genero
-                })
-                const cliente = await clienteNovo.save()
-                res.status(200).json({"cliente adicionado": cliente })
-            } else {
-                throw new Error('Email ja cadastrado no sistema')
-            }
+            if (testaEmail.length > 0) {
+               res.status(400).json({"message": "Email ja cadastrado", "erro": true})
+               return
+            } 
+            const clienteNovo = new Clientes({
+                nome: body.nome,
+                data_nascimento: body.data_nascimento,
+                cpf: body.cpf,
+                email: body.email,
+                senha: body.senha,
+                genero: body.genero
+            })
+            const cliente = await clienteNovo.save()
+            res.status(200).json({"cliente adicionado": cliente })
     
             
            
@@ -66,9 +75,9 @@ import  express  from "express";
         try {
             const {email, senha} = req.body
             const login = await Clientes.findOne({email: email, senha: senha})
-            if (login == null) {
-               throw new Error()
-                
+            if (!login) {
+               res.status(422).json({"message": "o usuario nao foi encontrado"})
+                return
             } 
             res.status(200).json({'mensagem': 'encontrei', 'cliente': login})
             
@@ -82,10 +91,14 @@ import  express  from "express";
         try {
             const id = req.params.id
             const body = req.body
-            await Clientes.updateOne({_id: id}, {$set: {nome: body.nome, idade: body.idade, cpf: body.cpf, email: body.email, senha: body.senha,
-            genero: body.genero, autorizacao: body.autorizacao}})
+            const response = await Clientes.updateOne({_id: id}, body)
+            console.log(response);
+            if (response.matchedCount === 0) {
+                res.status(422).json({"message": "usuario nao encontrado", "erro": true})
+                return
+            }
     
-            res.status(200).json({"msg": "cliente atualizado", "novos dados": body})
+            res.status(200).json({"message": "cliente atualizado", "cliente": body})
         } catch (error) {
             res.status(400).json({"erro": true, 'msg': error.message })
         }
